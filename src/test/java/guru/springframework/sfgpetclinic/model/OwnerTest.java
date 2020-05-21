ffd8be13b96113149e5eac44093e4d24a2f77fce
Guilherme Lodi <idd_glodi@uolinc.com>
2020-05-21 17:35:01 -0300

    JUnit Dependent Assertions

---
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -0,0 +1,25 @@
+package guru.springframework.sfgpetclinic.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OwnerTest {
+
+    @Test
+    void dependentAssertions() {
+        Owner owner = new Owner(1l, "Guilherme", "Lodi");
+        owner.setCity("Rio Preto");
+        owner.setTelephone("17998765432");
+
+        assertAll("Properties test",
+                () -> assertAll("Person properties",
+                        () -> assertEquals(owner.getFirstName(), "Guilherme", "First name failed"),
+                        () -> assertEquals(owner.getLastName(), "Lodi", "Last name failed")),
+                () -> assertAll("Owner properties",
+                        () -> assertEquals(owner.getCity(), "Rio Preto", "City failed"),
+                        () -> assertEquals(owner.getTelephone(), "17998765432", "Telephone failed"))
+        );
+    }
+
+}
