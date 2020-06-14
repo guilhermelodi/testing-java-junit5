ca10a28b3ce810535bb9c563153f76d551235f77
Guilherme Lodi <guilherme.tlodi@gmail.com>
2020-06-14 18:04:06 -0300

    JUnit Default Test Methods (using interface)

--- src/test/java/guru/springframework/sfgpetclinic/controllers/VetControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/VetControllerTest.java
@@ -8,7 +8,6 @@ import guru.springframework.sfgpetclinic.services.VetService;
 import guru.springframework.sfgpetclinic.services.map.SpecialityMapService;
 import guru.springframework.sfgpetclinic.services.map.VetMapService;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashSet;
